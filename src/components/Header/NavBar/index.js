20d624871f94c2d8c528f8d8765f280bac5a3050
hectormr <45437401+HectorMartinezResendiz@users.noreply.github.com>
2020-07-02 23:54:45 -0500

    <WIP> Changes to the header

--- src/components/Header/NavBar/index.js
+++ src/components/Header/NavBar/index.js
@@ -1,12 +1,17 @@
 import React from "react";
+import { IconAvatar } from "../../../assets/static/icon-avatar";
 import { Nav, Link } from "./styles";
 
 export const NavBar = () => {
   return (
     <Nav>
       <Link to="/about">Nosotros</Link>
+      <Link to="/agenda">Mi agenda</Link>
       <Link to="/signin">Registrarse</Link>
-      <Link to="/login">Iniciar sesión</Link>
+      <Link to="/login">
+        <IconAvatar fill="#DE18AD" /> Iniciar sesión
+      </Link>
+      <Link to="/logout">Cerar sesión</Link>
     </Nav>
   );
 };
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -5,15 +5,27 @@ export const HeaderStyle = styled.header`
   position: sticky;
   top: 0;
   /* Modelo de caja (Box-model) */
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding-left: 15px;
-  padding-right: 15px;
   height: 100px;
   /* Tipografía */
   /* Visual */
   background: #02b3b1;
   /* Misc */
   z-index: 999999;
+`;
+
+export const Wrap = styled.div`
+  /* Posicionamiento */
+  /* Modelo de caja (Box-model) */
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding-left: 15px;
+  padding-right: 15px;
+  margin: 0 auto;
+  height: inherit;
+  width: 1200px;
+  max-width: 1200px;
+  /* Tipografía */
+  /* Visual */
+  /* Misc */
 `;
