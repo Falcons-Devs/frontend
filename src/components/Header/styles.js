124c0aed7eff7156cc0abc441cb4b049615a5c7d
hectormr <45437401+HectorMartinezResendiz@users.noreply.github.com>
2020-07-03 00:16:24 -0500

    <WIP> Translation of comments into English

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -4,15 +4,9 @@ export const HeaderStyle = styled.header`
   /* Positioning */
   position: sticky;
   top: 0;
-<<<<<<< HEAD
   /* (Box-model) */
   height: 100px;
   /* Typography */
-=======
-  /* Modelo de caja (Box-model) */
-  height: 100px;
-  /* Tipografía */
->>>>>>> 20d624871f94c2d8c528f8d8765f280bac5a3050
   /* Visual */
   background: #02b3b1;
   /* Misc */
@@ -20,13 +14,8 @@ export const HeaderStyle = styled.header`
 `;
 
 export const Wrap = styled.div`
-<<<<<<< HEAD
   /* Positioning */
   /* (Box-model) */
-=======
-  /* Posicionamiento */
-  /* Modelo de caja (Box-model) */
->>>>>>> 20d624871f94c2d8c528f8d8765f280bac5a3050
   display: flex;
   align-items: center;
   justify-content: space-between;
@@ -36,11 +25,7 @@ export const Wrap = styled.div`
   height: inherit;
   width: 1200px;
   max-width: 1200px;
-<<<<<<< HEAD
   /* Typography */
-=======
-  /* Tipografía */
->>>>>>> 20d624871f94c2d8c528f8d8765f280bac5a3050
   /* Visual */
   /* Misc */
 `;
