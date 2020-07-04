0245e039cdfc93219efaac84ae03fcc5898906b0
Jhon Carlos Colorado <8047140+jcolorado19@users.noreply.github.com>
2020-07-04 17:21:06 -0500

    <WIP> Page of registry is finish

--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -1,23 +1,12 @@
 import styled from "styled-components";
 
-export const FooterStyle = styled.footer`
-  /* Positioning */
-  /* Box-model */
-  height: 100%;
-  width: 100%;
-  /* Typography */
-  /* Visual */
-  /* Misc */
-  bottom: 0;
-`;
-
 export const Wrap = styled.div`
   /* Positioning */
   /* (Box-model) */
   padding-left: 15px;
   padding-right: 15px;
   margin: 0 auto;
-  height: inherit;
+  height: 60px;
   width: 1200px;
   max-width: 1200px;
   /* Typography */
@@ -26,3 +15,15 @@ export const Wrap = styled.div`
   border-radius: 10px 10px 0 0;
   /* Misc */
 `;
+
+export const FooterStyle = styled.footer`
+  /* Positioning */
+  /* Box-model */
+  height: inherit;
+  width: 100%;
+  /* Typography */
+  /* Visual */
+  /* Misc */
+  bottom: 0;
+`;
+
---
+++ src/components/Main/Home/Signin/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { IconAccountCircle } from '../../../../assets/static/icon-accountCircle';
+import { IconEmail } from '../../../../assets/static/icon-email';
+import { IconPassword } from '../../../../assets/static/icon-password';
+import { IconFacebook } from '../../../../assets/static/icon-facebook';
+import { IconGoogle } from '../../../../assets/static/icon-google';
+import { Wrap, SectionForm } from './styles';
+
+export const Signin = () => {
+    return (
+        <Wrap>
+            <SectionForm>
+                <h2>Registrate</h2>
+                <form >
+                    <div>
+                        <label htmlFor="nombre"><IconAccountCircle width="50px" height="50px" fill="#DE18AD"/></label>
+                        <input type="text" id="nombre" placeholder="Nombre"/>
+                    </div>
+                    <div>
+                        <label htmlFor="email"> <IconEmail width="50px" height="50px" fill="#DE18AD" /> </label>
+                        <input type="text" id="email" placeholder="Coreo electronico"/>
+                    </div>
+                    <div>
+                        <label htmlFor="password"> <IconPassword width="50px" height="50px" fill="#DE18AD" /> </label>
+                        <input type="text" id="password" placeholder="password"/>
+                    </div>
+                    <button>Registrarse</button>
+                </form>
+                <div>
+                    <p>O registrate con:</p>
+                    <IconFacebook />
+                    <IconGoogle />
+                </div>
+            </SectionForm>
+        </Wrap>
+    )
+}
