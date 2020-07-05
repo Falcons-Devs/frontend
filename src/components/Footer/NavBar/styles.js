8f462d65fe736fa3a59285a4b7082c9510bb874a
Erickowski <daniel_perez_03081995@hotmail.com>
2020-07-05 15:32:46 -0500

    <REFACTOR> modify styles of Copyright in Footer Navbar

--- src/components/Footer/NavBar/styles.js
+++ src/components/Footer/NavBar/styles.js
@@ -47,4 +47,13 @@ export const Link = styled(LinkRouter)`
 
 export const P = styled.p`
   margin-left: 20px;
+  /* Typography */
+  font-family: "Italianno", -apple-system, BlinkMacSystemFont, "Segoe UI",
+    Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", system-ui,
+    sans-serif;
+  font-size: 32px;
+  font-weight: 400;
+  color: #fff;
+  letter-spacing: 0.25px;
+  line-height: 1.28571;
 `;
