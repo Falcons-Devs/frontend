8baf27587f8f278696b3f8ce220d06c81d24c1ef
hectormr <45437401+HectorMartinezResendiz@users.noreply.github.com>
2020-07-06 00:50:00 -0500

    <REFACTOR> The ImagesWithProcess component is redesigned by adding the link throughout the components, increasing the border-radius, adding the correct font size and placing a hatch.

--- src/components/ImagesWithProcess/styles.js
+++ src/components/ImagesWithProcess/styles.js
@@ -1,27 +1,68 @@
 import styled from "styled-components";
 import { Link as LinkRouter } from "@reach/router";
 
-export const Image = styled.div`
+export const Link = styled(LinkRouter)`
+  /* Positioning */
+  /* Box-model */
+  width: ${(props) => props.width || "300px"};
+  height: ${(props) => props.height || "368px"};
+  /* Typography */
+  outline: none;
+  /* Visual */
+  background-image: url('${(props) => props.cover}');
+  background-position: center center;
+  background-repeat: no-repeat;
+  background-color: #B0B0B0;
+  background-size: cover;
+  border-radius: 69px;
+  box-shadow: 0 2px 4px rgba(0,0,0,0.15);
+  color: #fff;
+  display: block;
+  overflow: hidden;
   position: relative;
-  width: ${(props) => props.widthImage || "300px"};
+  transition: background-color .2s cubic-bezier(0.4, 0, 0.2, 1),box-shadow .3s cubic-bezier(0.4, 0, 0.2, 1),color .2s cubic-bezier(0.4, 0, 0.2, 1),fill .2s cubic-bezier(0.4, 0, 0.2, 1);
+  /* Misc */
 
-  img {
-    height: ${(props) => props.heightImage || "368px"};
-    width: 100%;
-  }
-
-  p {
+  &:hover {
+    box-shadow: 0 4px 5px rgba(0,0,0,0.4);
   }
 `;
 
-export const Link = styled(LinkRouter)`
+export const Article = styled.article`
+  /* Positioning */
   position: absolute;
-  bottom: 4px;
+  top: 0;
+  right: 0;
+  bottom: 0;
   left: 0;
-  width: 100%;
-  padding: 20px 0;
-  border-radius: 0 0 35px 35px;
-  background-color: rgba(252, 252, 252, 0.9);
-  color: #de18ad;
+  /* Box-model */
+  /* Typography */
+  /* Visual */
+  /* Misc */
+`;
+
+export const Section = styled.section`
+  /* Positioning */
+  position: absolute;
+  bottom: 0;
+  /* Box-model */
   text-align: center;
+  padding: 20px 10px;
+  width: 100%;
+  /* Typography */
+  /* Visual */
+  background-color: rgba(252, 252, 252, 0.9);
+  /* Misc */
+`;
+
+export const H2 = styled.h2`
+  /* Positioning */
+  /* Box-model */
+  /* Typography */
+  color: #de18ad;
+  font-size: 32px;
+  font-weight: 300;
+  line-height: 1.3;
+  /* Visual */
+  /* Misc */
 `;
