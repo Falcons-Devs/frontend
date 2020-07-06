f9d0ff75c9422361ec0a0d96c95a50b8fd574e51
Erickowski <daniel_perez_03081995@hotmail.com>
2020-07-06 14:35:05 -0500

    <WIP> Admin Users Page, left Table

--- src/components/Main/Pages/Admin/Users/styles.js
+++ src/components/Main/Pages/Admin/Users/styles.js
@@ -1,21 +1,27 @@
 import styled from "styled-components";
 
+export const Wrap = styled.div`
+  /* Positioning */
+  /* (Box-model) */
+  margin: 20px auto;
+  width: 1200px;
+  max-width: 1200px;
+  padding: 0px 20px;
+  /* Typography */
+  /* Visual */
+`;
+
 export const Container = styled.div`
   display: grid;
-  grid-template-columns: repeat(1fr, 3);
+  grid-template-columns: 1fr;
   grid-template-rows: 1fr 1fr;
 `;
-export const ArrowButton = styled.div`
+export const Main = styled.div`
   grid-column: 1 / 2;
   grid-row: 1 / 2;
-`;
-export const Title = styled.div`
-  grid-column: 2 / 3;
-  grid-row: 1 / 2;
-`;
-export const CreateButton = styled.div`
-  grid-column: 3 / 4;
-  grid-row: 1 / 2;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 `;
 export const Table = styled.div`
   grid-column: 1 / 4;
