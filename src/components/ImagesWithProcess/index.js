b2027b7634c97e0842faa047df30ac94ab8ff66a
Erickowski <daniel_perez_03081995@hotmail.com>
2020-07-05 21:34:15 -0500

    <REFACTOR> dynamic route in ImageWithProcess

--- src/components/ImagesWithProcess/index.js
+++ src/components/ImagesWithProcess/index.js
@@ -6,14 +6,14 @@ import Hero_card3 from "../../assets/img/hero_card3.jpg";
 import Hero_card4 from "../../assets/img/hero_card4.jpg";
 import { Image, Link } from "./styles";
 
-export const ImagesWithProcess = ({ nameImage, alt, width, height }) => {
+export const ImagesWithProcess = ({ nameImage, alt, width, height, to }) => {
   return (
     <Image widthImage={width} heightImage={height}>
       {nameImage == 1 && <img src={Hero_card1} alt={alt} />}
       {nameImage == 2 && <img src={Hero_card2} alt={alt} />}
       {nameImage == 3 && <img src={Hero_card3} alt={alt} />}
       {nameImage == 4 && <img src={Hero_card4} alt={alt} />}
-      <Link to="/miagenda"> {alt} </Link>
+      <Link to={to}> {alt} </Link>
     </Image>
   );
 };
