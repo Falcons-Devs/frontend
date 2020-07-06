0e9619a68d9e0227a6337fad67e7e426ce376a4a
Erickowski <daniel_perez_03081995@hotmail.com>
2020-07-06 13:07:21 -0500

    <REFACTOR> Personalize Profile Picture, Social Networks and GitHub in About

--- src/components/CardMembers/index.js
+++ src/components/CardMembers/index.js
@@ -1,24 +1,55 @@
 import React from "react";
-
+import { Link } from "@reach/router";
 import { IconAccountCircle } from "../../assets/static/icon-accountCircle";
-// import { IconAccountCircle } from "../../";
-import { Article } from "./styles";
+import { Article, Image } from "./styles";
+import { IconGitHub } from "../../assets/static/icon-github";
+import { IconLinkedIn } from "../../assets/static/icon-linkedin";
+import { IconTwitter } from "../../assets/static/icon-twitter";
 
-export const CardMembers = ({ nameMember, position }) => {
+export const CardMembers = ({
+  nameMember,
+  position,
+  img,
+  github,
+  linkedin,
+  twitter,
+}) => {
   return (
-    <>
-      <Article>
-        <article>
-          <span>
+    <Article>
+      <article>
+        <span>
+          {img ? (
+            <Image src={img} />
+          ) : (
             <IconAccountCircle width="110px" height="110px" fill="#DE18AD" />
-          </span>
-          <div id="name">
-            <p> {nameMember} </p>
-            <div></div>
-          </div>
-          <p id="project"> {position} </p>
-        </article>
-      </Article>
-    </>
+          )}
+        </span>
+        <div id="name">
+          <p> {nameMember} </p>
+          {github ? (
+            <a href={github} target="_blank">
+              <IconGitHub width="25px" height="25px" fill="#DE18AD" />
+            </a>
+          ) : (
+            ""
+          )}
+          {linkedin ? (
+            <a href={linkedin} target="_blank">
+              <IconLinkedIn width="25px" height="25px" />
+            </a>
+          ) : (
+            ""
+          )}
+          {twitter ? (
+            <a href={twitter} target="_blank">
+              <IconTwitter width="25px" height="25px" />
+            </a>
+          ) : (
+            ""
+          )}
+        </div>
+        <p id="project"> {position} </p>
+      </article>
+    </Article>
   );
 };
