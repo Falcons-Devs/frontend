333fdc8838503362d68f29e72853001f42a37664
Erickowski <daniel_perez_03081995@hotmail.com>
2020-07-07 13:16:03 -0500

    <WIP> Home Admin Page

--- src/components/Main/Pages/Admin/Home/styles.js
+++ src/components/Main/Pages/Admin/Home/styles.js
@@ -10,16 +10,29 @@ export const Wrap = styled.div`
   /* Typography */
   /* Visual */
   /* Misc */
+  /* Responsive */
+  @media (max-width: 768px) {
+    width: 85vw;
+    height: 80.5vh;
+  }
 `;
 
 export const Container = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr 1fr;
   grid-template-rows: 1fr 1fr;
+  /* Responsive */
+  @media (max-width: 768px) {
+    grid-template-columns: 1fr 1fr;
+  }
 `;
 export const Clients = styled.div`
   grid-column: 1 / 2;
   grid-row: 1 / 3;
+  /* Responsive */
+  @media (max-width: 768px) {
+    grid-row: 1 / 2;
+  }
 `;
 export const Beautician = styled.div`
   grid-column: 2 / 3;
@@ -28,8 +41,17 @@ export const Beautician = styled.div`
 export const Procedures = styled.div`
   grid-column: 2 / 3;
   grid-row: 2 / 3;
+  /* Responsive */
+  @media (max-width: 768px) {
+    grid-column: 1 / 2;
+  }
 `;
 export const Users = styled.div`
   grid-column: 3 / 4;
   grid-row: 1 / 3;
+  /* Responsive */
+  @media (max-width: 768px) {
+    grid-column: 2 / 3;
+    grid-row: 2 / 3;
+  }
 `;
