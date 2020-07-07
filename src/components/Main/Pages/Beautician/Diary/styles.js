8fc3aadab818209841b848db0234c2e07038c6d0
Erickowski <daniel_perez_03081995@hotmail.com>
2020-07-07 09:36:39 -0500

    <REFACTOR> Adding Schedule to Diary Beautician page

--- src/components/Main/Pages/Beautician/Diary/styles.js
+++ src/components/Main/Pages/Beautician/Diary/styles.js
@@ -1,9 +1,20 @@
 import styled from "styled-components";
 
+export const Wrap = styled.div`
+  /* Positioning */
+  /* (Box-model) */
+  margin: 20px auto;
+  width: 1200px;
+  max-width: 1200px;
+  padding: 0px 20px;
+  /* Typography */
+  /* Visual */
+`;
+
 export const Container = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr;
-  grid-template-rows: 1fr 1fr;
+  grid-template-rows: 75px 1fr;
 `;
 export const ArrowButton = styled.div`
   margin-top: 20px;
