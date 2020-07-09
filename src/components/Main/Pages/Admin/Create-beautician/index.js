42302c06c08d813cb71e9c1da3946d257088655d
Ernesto Lazaro <ernestolazaro84@gmail.com>
2020-07-09 14:25:51 -0500

    FIX Admin-forms include a admin create beautician component too

--- src/components/Main/Pages/Admin/Create-beautician/index.js
+++ src/components/Main/Pages/Admin/Create-beautician/index.js
@@ -3,19 +3,11 @@ import {
   Container,
   Main,
   Hero,
-  Form,
-  CancelButton,
-  CreateButton,
-  SectionForm,
   Wrap,
-  Title,
 } from "./styles";
 import { NameStatus } from "../../../../NameStatus";
-import { Buttons } from "../../../../Buttons";
 import { HeaderImages } from "../../../../HeaderImages";
-import { IconEmail } from "../../../../../assets/static/icon-email";
-import { IconPassword } from "../../../../../assets/static/icon-password";
-import { IconAccountCircle } from "../../../../../assets/static/icon-accountCircle";
+import { AdminForms } from "../../../../AdminForms";
 import { Link } from "@reach/router";
 
 export const CreateBeautician = () => {
@@ -31,50 +23,9 @@ export const CreateBeautician = () => {
         <Hero>
           <HeaderImages numberImg="2" />
         </Hero>
-        <Form>
-          <SectionForm>
-            <form>
-              <div>
-                <label htmlFor="name">
-                  {" "}
-                  <IconAccountCircle
-                    width="50px"
-                    height="50px"
-                    fill="#DE18AD"
-                  />{" "}
-                </label>
-                <input type="text" id="name" placeholder="Nombre" />
-              </div>
-              <div>
-                <label htmlFor="email">
-                  {" "}
-                  <IconEmail width="50px" height="50px" fill="#DE18AD" />{" "}
-                </label>
-                <input type="text" id="email" placeholder="Coreo electronico" />
-              </div>
-              <div>
-                <label htmlFor="password">
-                  {" "}
-                  <IconPassword
-                    width="50px"
-                    height="50px"
-                    fill="#DE18AD"
-                  />{" "}
-                </label>
-                <input type="password" id="password" placeholder="Password" />
-              </div>
-            </form>
-            <Title>Crear esteticista</Title>
-          </SectionForm>
-        </Form>
-        <CancelButton>
-          <Link to="/admin-beautician">
-            <Buttons color="#DE3C48" value="Cancelar" />
-          </Link>
-        </CancelButton>
-        <CreateButton>
-          <Buttons color="#2DD881" value="Confirmar" />
-        </CreateButton>
+       
+        <AdminForms actionUser="Crear Esteticista" type="Usuario" buttonAction="Crear Esteticista"/>
+
       </Container>
     </Wrap>
   );
