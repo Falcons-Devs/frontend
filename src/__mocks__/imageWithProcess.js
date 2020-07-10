416eedf361fb6cb308e5f33e05f4da71d121518d
Erickowski <daniel_perez_03081995@hotmail.com>
2020-07-10 14:47:00 -0500

    <TEST> Solved conflicts of images and some testings

---
+++ src/__mocks__/imageWithProcess.js
@@ -0,0 +1,7 @@
+export const nameImage = "Imagen";
+export const alt = "Descripcion";
+export const width = "600px";
+export const height = "300px";
+export const to = "/";
+export const responsivetablet = "true";
+export const responsivemobile = "true";
