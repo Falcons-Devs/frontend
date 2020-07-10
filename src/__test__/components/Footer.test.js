5d5865cd4984eba4a14b9da2b3f5e09508d92b9d
Erickowski <daniel_perez_03081995@hotmail.com>
2020-07-09 21:51:55 -0500

    <WIP> Testing footer

--- src/__test__/components/Footer.test.js
+++ src/__test__/components/Footer.test.js
@@ -2,10 +2,26 @@ import React from "react";
 import { mount } from "enzyme";
 
 import { Footer } from "../../components/Footer";
+import { Social } from "../../components/Footer/Social";
+import { NavBarFooter } from "../../components/Footer/NavBar";
 
 describe("<Footer />", () => {
   test("Render del componente Footer", () => {
     const footer = mount(<Footer />);
     expect(footer.length).toEqual(1);
+  });
+});
+
+describe("<Social />", () => {
+  test("Render del componente Social", () => {
+    const social = mount(<Social />);
+    expect(social.length).toEqual(1);
+  });
+});
+
+describe("<NavBarFooter />", () => {
+  test("Render del componente Social", () => {
+    const navbar = mount(<NavBarFooter />);
+    expect(navbar.length).toEqual(1);
   });
 });
