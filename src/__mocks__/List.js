b40d4475135b17462959863dc2be18b1a65cedbb
Erickowski <daniel_perez_03081995@hotmail.com>
2020-07-10 13:29:20 -0500

    <BUGFIX> Solved List Testing

--- src/__mocks__/List.js
+++ src/__mocks__/List.js
@@ -1,4 +1,4 @@
-export const list = [1, 2, 3, 4];
+export const list = ["Procedimiento 1", "Procedimiento 2", "Procedimiento 3"];
 export const title = "Title";
 export const full = "true";
-export const topic = "Topic";
+export const topic = "Procedures ";
