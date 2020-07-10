a8ce526f1c5ae4ce1cdb1a0ef908ca83591ceb96
Erickowski <daniel_perez_03081995@hotmail.com>
2020-07-10 16:38:46 -0500

    <TEST> Testing Component Schedule

---
+++ src/__mocks__/NameStatus.js
@@ -0,0 +1,2 @@
+export const title = "Titulo";
+export const to = "/";
--- src/__test__/components/NameStatus.test.js
+++ src/__test__/components/NameStatus.test.js
@@ -2,9 +2,11 @@ import React from "react";
 import { mount } from "enzyme";
 import { NameStatus } from "../../components/NameStatus";
 
+import { title, to } from "../../__mocks__/NameStatus";
+
 describe("<NameStatus />", () => {
   test("Component Name Status Rendering", () => {
-    const namestatus = mount(<NameStatus title="e" to="/" />);
+    const namestatus = mount(<NameStatus title={title} to={to} />);
     expect(namestatus.length).toEqual(1);
   });
 });
