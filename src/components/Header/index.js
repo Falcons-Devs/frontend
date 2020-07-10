60c395af8ffafd44679674afcf8f6906d305eade
hectormr <45437401+HectorMartinezResendiz@users.noreply.github.com>
2020-07-09 20:43:57 -0500

    <FIX> Correct error presented in the logo, and increase the size of the menu buttons to have more space and to be able to click.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -8,7 +8,7 @@ import { LogoOCS } from "../../assets/static/logo-ocs";
 export const Header = () => {
   let logo = <LogoOCSF width="260px" />;
   if (screen.width <= 768) {
-    logo = <LogoOCS width="90px" height="65px" />;
+    logo = <LogoOCS width="300px" height="62px" />;
   }
   if (screen.width <= 375) {
     logo = <LogoOCS width="50px" height="50px" />;
