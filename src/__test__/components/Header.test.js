5a0c76e46b662add343b5e5303c95f1a24f9481a
Erickowski <daniel_perez_03081995@hotmail.com>
2020-07-10 23:05:59 -0500

    <TEST> Final testings

--- src/__test__/components/Header.test.js
+++ src/__test__/components/Header.test.js
@@ -3,22 +3,23 @@ import { mount } from "enzyme";
 
 import Context from "../../Context";
 import { Header } from "../../components/Header";
-import { Link } from "@reach/router";
-import { NavBar } from "../../components/Header/NavBar";
-import { HeaderStyle, Wrap } from "../../components/Header/styles";
 
 describe("<Header />", () => {
   test("Render of Header component", () => {
     const header = mount(
       <Context.Provider>
-        <Header>
-          <HeaderStyle>
-            <Wrap>
-              <Link to="/" aria-label="Logo de OCS"></Link>
-              <NavBar />
-            </Wrap>
-          </HeaderStyle>
-        </Header>
+        <Header />
+      </Context.Provider>
+    );
+    expect(header.length).toEqual(1);
+  });
+});
+
+describe("<Header />", () => {
+  test("Render of Header component for Public", () => {
+    const header = mount(
+      <Context.Provider value={{ userType: "Public" }}>
+        <Header />
       </Context.Provider>
     );
     expect(header.length).toEqual(1);
