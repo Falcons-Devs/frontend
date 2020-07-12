fb6b731c01f19b78de6d299fdee4d447ddcecbb7
Erickowski <daniel_perez_03081995@hotmail.com>
2020-07-12 01:42:12 -0500

    <WIP> Authetication login

--- src/components/Main/Home/Login/index.js
+++ src/components/Main/Home/Login/index.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "@reach/router";
-import AuthState from "../../../../context/Authentication/authState";
+import Axios from "axios";
+
+// import AuthState from "../../../../context/Authentication/authState";
 // import Facebook from "../../../../apk/Facebook";
 // import Google from "../../../../apk/Google";
 // Import the icons svg
@@ -11,8 +13,11 @@ import { Buttons } from "../../../Buttons";
 
 // Import useEffect So that when the user changes the page it goes to the top
 import { Wrap, SectionForm } from "./styles";
+import Context from "../../../../Context";
 
 export const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   let iconEmail = <IconEmail width="50px" height="50px" fill="#DE18AD" />;
   let iconPassword = <IconPassword width="50px" height="50px" fill="#DE18AD" />;
 
@@ -23,29 +28,55 @@ export const Login = () => {
   }
 
   return (
-    <AuthState>
-      <Wrap>
-        <SectionForm>
-          <h2>Iniciar sesión</h2>
-          <form>
-            <div>
-              <label htmlFor="email"> {iconEmail} </label>
-              <input type="text" id="email" placeholder="Coreo electronico" />
-            </div>
-            <div>
-              <label htmlFor="password">{iconPassword}</label>
-              <input type="text" id="password" placeholder="Password" />
-            </div>
-            {/* Componente buton. Se envia dos propiedades para su uso */}
-            <Buttons value="Iniciar sesión" color="#DE18AD" />
-          </form>
-          <div>
-            <p>
-              ¿Cliente nuevo? <Link to="/signin">Registrate aquí</Link>
-            </p>
-          </div>
-        </SectionForm>
-      </Wrap>
-    </AuthState>
+    <Context.Consumer>
+      {({ changeToken }) => {
+        const auth = async (e) => {
+          e.preventDefault();
+          const result = await Axios.post("http://104.198.182.133/auth/login", {
+            email: email,
+            password: password,
+          });
+          changeToken(result.data.body);
+        };
+        return (
+          <Wrap>
+            <SectionForm>
+              <h2>Iniciar sesión</h2>
+              <form>
+                <div>
+                  <label htmlFor="email"> {iconEmail} </label>
+                  <input
+                    type="text"
+                    id="email"
+                    placeholder="Correo electronico"
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </div>
+                <div>
+                  <label htmlFor="password">{iconPassword}</label>
+                  <input
+                    type="password"
+                    id="password"
+                    placeholder="Password"
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </div>
+                {/* Componente buton. Se envia dos propiedades para su uso */}
+                <Buttons
+                  value="Iniciar sesión"
+                  color="#DE18AD"
+                  onClick={(e) => auth(e)}
+                />
+              </form>
+              <div>
+                <p>
+                  ¿Cliente nuevo? <Link to="/signin">Registrate aquí</Link>
+                </p>
+              </div>
+            </SectionForm>
+          </Wrap>
+        );
+      }}
+    </Context.Consumer>
   );
 };
